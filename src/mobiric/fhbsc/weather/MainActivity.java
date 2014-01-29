5dec9bf5cd96e0c8a709c67bb8e4ba74acc52302
Richard Le Mesurier <richard.lemesurier@gmail.com>
2014-01-29 20:44:10 +0200

    v0.1 - initial commit
    WebView based app to display the live weather feed from
    "http://www.fhbsc.co.za/fhbsc/weather/smartphone/index.html"

---
+++ src/mobiric/fhbsc/weather/MainActivity.java
@@ -0,0 +1,72 @@
+package mobiric.fhbsc.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity
+{
+	private static final String BASE_URL = "http://www.fhbsc.co.za/fhbsc/weather/smartphone/";
+	private static final String HOME_PAGE = BASE_URL + "index.html";
+	WebView webView;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+		webView = (WebView) findViewById(R.id.webView1);
+
+		// this site uses javascript to format the page
+		webView.getSettings().setJavaScriptEnabled(true);
+
+		webView.setWebViewClient(new WebViewClient()
+		{
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url)
+			{
+				// load inside the webview
+				view.loadUrl(url);
+				return true;
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url)
+			{
+				// hide title on home page
+				if (HOME_PAGE.equals(url))
+				{
+					String jsHideTitle =
+							"  var allElements = document.getElementsByTagName('*');"
+									+ "  for (var i = 0; i < allElements.length; i++)"
+									+ "  {"
+									+ "    if (allElements[i].getAttribute('data-role')==\"header\")"
+									+ "    {" + "      allElements[i].style.display='none';"
+									+ "      break;" + "    }" + "  }";
+					view.loadUrl("javascript:" + jsHideTitle);
+				}
+
+				super.onPageFinished(view, url);
+			}
+		});
+		webView.loadUrl(HOME_PAGE);
+	}
+
+	/**
+	 * Allow back button to handle navigation.
+	 */
+	@Override
+	public void onBackPressed()
+	{
+		if (webView.canGoBack())
+		{
+			webView.goBack();
+		}
+		else
+		{
+			super.onBackPressed();
+		}
+	}
+}
